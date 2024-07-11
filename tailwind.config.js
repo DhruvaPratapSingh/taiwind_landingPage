5134829a474c34fe97256a45af617b64c04ba49f
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2024-07-11 23:41:20 +0530

    ready to deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3238f2',
+      },
+      fontFamily: {
+        'display': ['Poppins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
